7f3bc9800cfbf77cbf958cfdfac925c57c266027
S Vijayaprasad <svijaym6@gmail.com>
2023-10-19 17:25:25 +0530

    form behavior changes

--- voice/src/components/Form.jsx
+++ voice/src/components/Form.jsx
@@ -55,135 +55,138 @@ const Form = () => {
         p="20px"
         marginTop="50px"
       >
-        <div>
-          <FormLabel>First Name</FormLabel>
-          <br />
-          <Flex>
-            <Input
-              type="text"
-              name="firstName"
-              value={formData.firstName}
-              onChange={handleInputChange}
-              placeholder="First Name"
-            />
-            <Text>{isRecording ? "Stop Recording" : "Start Recording"}</Text>
-            <Button onClick={handleRecording}>
-              <BsMic />
-            </Button>
-          </Flex>
-          <Text>{isRecording ? "Listening..." : "Not Listening"}</Text>
-        </div>
-        <div>
-          <FormLabel>Last Name</FormLabel>
-          <br />
-          <Flex>
-            <Input
-              type="text"
-              name="lastName"
-              value={formData.lastName}
-              onChange={handleInputChange}
-              placeholder="Last Name"
-            />
-            <Text>{isRecording ? "Stop Recording" : "Start Recording"}</Text>
-            <Button onClick={handleRecording}>
-              <BsMic />
-            </Button>
-          </Flex>
-          <Text>{isRecording ? "Listening..." : "Not Listening"}</Text>
-        </div>
-        <div>
-          <FormLabel>State</FormLabel>
-          <br />
-          <Flex>
-            <Input
-              type="text"
-              name="state"
-              value={formData.state}
-              onChange={handleInputChange}
-              placeholder="State"
-            />
-            <Text>{isRecording ? "Stop Recording" : "Start Recording"}</Text>
-            <Button onClick={handleRecording}>
-              <BsMic />
-            </Button>
-          </Flex>
-          <Text>{isRecording ? "Listening..." : "Not Listening"}</Text>
-        </div>
-        <div>
-          <FormLabel>District</FormLabel>
-          <br />
-          <Flex>
-            <Input
-              type="text"
-              name="district"
-              value={formData.district}
-              onChange={handleInputChange}
-              placeholder="District"
-            />
-            <Text>{isRecording ? "Stop Recording" : "Start Recording"}</Text>
-            <Button onClick={handleRecording}>
-              <BsMic />
-            </Button>
-          </Flex>
-          <Text>{isRecording ? "Listening..." : "Not Listening"}</Text>
-        </div>
-        <div>
-          <FormLabel>Village</FormLabel>
-          <br />
-          <Flex>
-            <Input
-              type="text"
-              name="village"
-              value={formData.village}
-              onChange={handleInputChange}
-              placeholder="Village"
-            />
-            <Text>{isRecording ? "Stop Recording" : "Start Recording"}</Text>
-            <Button onClick={handleRecording}>
-              <BsMic />
-            </Button>
-          </Flex>
-          <Text>{isRecording ? "Listening..." : "Not Listening"}</Text>
-        </div>
-        <div>
-          <FormLabel>PAN Number</FormLabel>
-          <br />
-          <Flex>
-            <Input
-              type="number"
-              name="panNumber"
-              value={formData.panNumber}
-              onChange={handleInputChange}
-              placeholder="PAN Number"
-            />
-            <Text>{isRecording ? "Stop Recording" : "Start Recording"}</Text>
-            <Button onClick={handleRecording}>
-              <BsMic />
-            </Button>
-          </Flex>
-          <Text>{isRecording ? "Listening..." : "Not Listening"}</Text>
-        </div>
-        <div>
-          <FormLabel>Aadhaar Number</FormLabel>
-          <br />
-          <Flex>
-            <Input
-              type="number"
-              name="aadhaarNumber"
-              value={formData.aadhaarNumber}
-              onChange={handleInputChange}
-              placeholder="Aadhaar Number"
-            />
-            <Text>{isRecording ? "Stop Recording" : "Start Recording"}</Text>
-            <Button onClick={handleRecording}>
-              <BsMic />
-            </Button>
-          </Flex>
-          <Text>{isRecording ? "Listening..." : "Not Listening"}</Text>
-        </div>
-        <Button mt={4} colorScheme="teal" type="submit" onClick={handleSubmit}>
-          Submit
-        </Button>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <FormLabel>First Name</FormLabel>
+            <br />
+            <Flex>
+              <Input
+                type="text"
+                name="firstName"
+                value={formData.firstName}
+                onChange={handleInputChange}
+                placeholder="First Name"
+              />
+              <Text>{isRecording ? "Stop Recording" : "Start Recording"}</Text>
+              <Button onClick={handleRecording}>
+                <BsMic />
+              </Button>
+            </Flex>
+            <Text>{isRecording ? "Listening..." : "Not Listening"}</Text>
+          </div>
+          <div>
+            <FormLabel>Last Name</FormLabel>
+            <br />
+            <Flex>
+              <Input
+                type="text"
+                name="lastName"
+                value={formData.lastName}
+                onChange={handleInputChange}
+                placeholder="Last Name"
+              />
+              <Text>{isRecording ? "Stop Recording" : "Start Recording"}</Text>
+              <Button onClick={handleRecording}>
+                <BsMic />
+              </Button>
+            </Flex>
+            <Text>{isRecording ? "Listening..." : "Not Listening"}</Text>
+          </div>
+          <div>
+            <FormLabel>State</FormLabel>
+            <br />
+            <Flex>
+              <Input
+                type="text"
+                name="state"
+                value={formData.state}
+                onChange={handleInputChange}
+                placeholder="State"
+              />
+              <Text>{isRecording ? "Stop Recording" : "Start Recording"}</Text>
+              <Button onClick={handleRecording}>
+                <BsMic />
+              </Button>
+            </Flex>
+            <Text>{isRecording ? "Listening..." : "Not Listening"}</Text>
+          </div>
+          <div>
+            <FormLabel>District</FormLabel>
+            <br />
+            <Flex>
+              <Input
+                type="text"
+                name="district"
+                value={formData.district}
+                onChange={handleInputChange}
+                placeholder="District"
+              />
+              <Text>{isRecording ? "Stop Recording" : "Start Recording"}</Text>
+              <Button onClick={handleRecording}>
+                <BsMic />
+              </Button>
+            </Flex>
+            <Text>{isRecording ? "Listening..." : "Not Listening"}</Text>
+          </div>
+          <div>
+            <FormLabel>Village</FormLabel>
+            <br />
+            <Flex>
+              <Input
+                type="text"
+                name="village"
+                value={formData.village}
+                onChange={handleInputChange}
+                placeholder="Village"
+              />
+              <Text>{isRecording ? "Stop Recording" : "Start Recording"}</Text>
+              <Button onClick={handleRecording}>
+                <BsMic />
+              </Button>
+            </Flex>
+            <Text>{isRecording ? "Listening..." : "Not Listening"}</Text>
+          </div>
+          <div>
+            <FormLabel>PAN Number</FormLabel>
+            <br />
+            <Flex>
+              <Input
+                type="number"
+                name="panNumber"
+                value={formData.panNumber}
+                onChange={handleInputChange}
+                placeholder="PAN Number"
+              />
+              <Text>{isRecording ? "Stop Recording" : "Start Recording"}</Text>
+              <Button onClick={handleRecording}>
+                <BsMic />
+              </Button>
+            </Flex>
+            <Text>{isRecording ? "Listening..." : "Not Listening"}</Text>
+          </div>
+          <div>
+            <FormLabel>Aadhaar Number</FormLabel>
+            <br />
+            <Flex>
+              <Input
+                type="number"
+                name="aadhaarNumber"
+                value={formData.aadhaarNumber}
+                onChange={handleInputChange}
+                placeholder="Aadhaar Number"
+              />
+              <Text>{isRecording ? "Stop Recording" : "Start Recording"}</Text>
+              <Button onClick={handleRecording}>
+                <BsMic />
+              </Button>
+            </Flex>
+            <Text>{isRecording ? "Listening..." : "Not Listening"}</Text>
+          </div>
+
+          <Button mt={2} colorScheme="teal" type="submit">
+            Submit
+          </Button>
+        </form>
       </Box>
     </div>
   );
